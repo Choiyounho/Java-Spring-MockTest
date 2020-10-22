4d1f2cbbd6b9c26e391284fb4a90dc78736932d2
Choiyounho <maxosa78@gmail.com>
2020-10-22 16:46:10 +0900

    [feat] : BDD 스타일 Mockito 구현

--- src/test/java/com/example/mock/study/StudyServiceTest.java
+++ src/test/java/com/example/mock/study/StudyServiceTest.java
@@ -14,19 +14,24 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
 
-    @Mock MemberService memberService;
-    @Mock StudyRepository studyRepository;
+    @Mock
+    MemberService memberService;
+    @Mock
+    StudyRepository studyRepository;
 
     @Test
+    @DisplayName("BDD 스타일 구현")
     void createStudyService() {
+        // Given
         StudyService studyService = new StudyService(memberService, studyRepository);
         assertNotNull(studyService);
-
 
         Member member = new Member();
         member.setId(1L);
@@ -34,17 +39,19 @@ class StudyServiceTest {
 
         Study study = new Study(10, "테스트");
 
-        when(memberService.findById(1L))
-                .thenReturn(Optional.of(member));
-        when(studyRepository.save(study)).thenReturn(study);
+        given(memberService.findById(1L))
+                .willReturn(Optional.of(member));
 
+        given(studyRepository.save(study))
+                .willReturn(study);
 
+        // When
         studyService.createNewStudy(1L, study);
 
+        // Then
         assertEquals(member, study.getOwner());
-
-        verify(memberService, times(1)).notify(study);
-        verifyNoMoreInteractions(memberService);
+        then(memberService).should(times(1)).notify(study);
+        then(memberService).shouldHaveNoInteractions();
     }
 
 }
